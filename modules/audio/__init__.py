beb695e31fc55eb7af629f91c0e1578d8e801255
Gabriel <gabriels.meleiro@gmail.com>
2026-01-28 23:24:49 +0100

    export encoder and packager

--- modules/audio/__init__.py
+++ modules/audio/__init__.py
@@ -5,6 +5,13 @@ Audio processing, encoding, and M4B packaging.
 """
 
 from .processor import AudioProcessor
+from .encoder import AudioEncoder
+from .packager import M4BPackager, AudiobookMetadata, ChapterMarker
 
-__all__ = ["AudioProcessor"]
-
+__all__ = [
+    "AudioProcessor",
+    "AudioEncoder", 
+    "M4BPackager",
+    "AudiobookMetadata",
+    "ChapterMarker",
+]
