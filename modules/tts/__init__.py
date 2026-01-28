8d9de37f08b3148b766a58938c0dd215d55e5e72
Gabriel <gabriels.meleiro@gmail.com>
2026-01-28 01:42:06 +0100

    export chunker and memory utilities

--- modules/tts/__init__.py
+++ modules/tts/__init__.py
@@ -5,5 +5,19 @@ Text-to-Speech engine using Kokoro-82M via MLX.
 """
 
 from .engine import TTSEngine, TTSConfig, list_voices, KOKORO_VOICES
+from .chunker import TextChunker, ChunkConfig, chunk_text
+from .memory import VRAMManager, clear_vram, get_memory_stats
 
-__all__ = ["TTSEngine", "TTSConfig", "list_voices", "KOKORO_VOICES"]
+__all__ = [
+    "TTSEngine", 
+    "TTSConfig", 
+    "list_voices", 
+    "KOKORO_VOICES",
+    "TextChunker",
+    "ChunkConfig",
+    "chunk_text",
+    "VRAMManager",
+    "clear_vram",
+    "get_memory_stats",
+]
+
