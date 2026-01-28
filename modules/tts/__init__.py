8440dcdc7315b7d1faec790aed0234a377d0f646
Gabriel <gabriels.meleiro@gmail.com>
2026-01-28 16:24:34 +0100

    export text cleaner

--- modules/tts/__init__.py
+++ modules/tts/__init__.py
@@ -7,6 +7,7 @@ Text-to-Speech engine using Kokoro-82M via MLX.
 from .engine import TTSEngine, TTSConfig, list_voices, KOKORO_VOICES
 from .chunker import TextChunker, ChunkConfig, chunk_text
 from .memory import VRAMManager, clear_vram, get_memory_stats
+from .cleaner import TextCleaner, CleanerConfig
 
 __all__ = [
     "TTSEngine", 
@@ -19,5 +20,6 @@ __all__ = [
     "VRAMManager",
     "clear_vram",
     "get_memory_stats",
+    "TextCleaner",
+    "CleanerConfig",
 ]
-
