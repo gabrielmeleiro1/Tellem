081d7e6525e1aa26bd0f334b7814297abb4212c1
Gabriel <gabriels.meleiro@gmail.com>
2026-01-29 00:37:06 +0100

    export progress tracking components

--- modules/ui/__init__.py
+++ modules/ui/__init__.py
@@ -4,5 +4,32 @@ UI Module
 Custom Streamlit components and styling utilities.
 """
 
-# Exports will be added in Phase 4
-__all__ = []
+from .progress import (
+    ProcessingStage,
+    ChapterProgress,
+    ConversionProgress,
+    init_progress_state,
+    get_progress,
+    reset_progress,
+    set_chapters,
+    update_chapter_progress,
+    set_current_stage,
+    render_chapter_progress,
+    render_stage_indicator,
+    render_compact_progress,
+)
+
+__all__ = [
+    "ProcessingStage",
+    "ChapterProgress",
+    "ConversionProgress",
+    "init_progress_state",
+    "get_progress",
+    "reset_progress",
+    "set_chapters",
+    "update_chapter_progress",
+    "set_current_stage",
+    "render_chapter_progress",
+    "render_stage_indicator",
+    "render_compact_progress",
+]
