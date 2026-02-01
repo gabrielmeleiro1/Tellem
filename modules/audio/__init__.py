0f8b6d9f2b61be33295237745bf64ea376ff22ae
Gabriel <gabriels.meleiro@gmail.com>
2026-02-01 15:14:36 +0100

    export buffer pool from audio module

--- modules/audio/__init__.py
+++ modules/audio/__init__.py
@@ -8,10 +8,32 @@ from .processor import AudioProcessor
 from .encoder import AudioEncoder
 from .packager import M4BPackager, AudiobookMetadata, ChapterMarker
 
+# Buffer pool for memory-efficient audio processing
+try:
+    from .buffer_pool import (
+        BufferPool,
+        PooledBuffer,
+        get_buffer_pool,
+        pooled_array,
+        BufferStats,
+    )
+    BUFFER_POOL_AVAILABLE = True
+except ImportError:
+    BUFFER_POOL_AVAILABLE = False
+
 __all__ = [
     "AudioProcessor",
-    "AudioEncoder", 
+    "AudioEncoder",
     "M4BPackager",
     "AudiobookMetadata",
     "ChapterMarker",
 ]
+
+if BUFFER_POOL_AVAILABLE:
+    __all__.extend([
+        "BufferPool",
+        "PooledBuffer",
+        "get_buffer_pool",
+        "pooled_array",
+        "BufferStats",
+    ])
