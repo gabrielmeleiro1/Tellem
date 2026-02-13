3f2772c4a1002852716675e6ab690c99a1c41574
Gabriel <gabriels.meleiro@gmail.com>
2026-02-13 11:31:54 +0100

    add screen exports

---
+++ modules/tui/screens/__init__.py
@@ -0,0 +1,9 @@
+"""
+TUI screens package.
+"""
+
+from modules.tui.screens.convert_modal import LaunchOptions, NewConversionModal
+from modules.tui.screens.dashboard import DashboardShell
+from modules.tui.screens.home import HomeScreen
+
+__all__ = ["DashboardShell", "HomeScreen", "LaunchOptions", "NewConversionModal"]
